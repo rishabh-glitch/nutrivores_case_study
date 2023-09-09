ab88335d7abad5a922b68ffd65fabf5d51111c21
rishabh-glitch <rishabhvikramsingh1998@gmail.com>
2023-09-09 13:28:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Main from "./components/Main";
+import ItemsAvailable from "./components/ItemsAvailable";
+import SignUp from "./components/SignUp";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={SignUp}></Route>
+            <Route path="/home" component={Main}></Route>
+            {/* <Route path="/restaurants" component={ItemsAvailable} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
